87a7fddd451e38d191d4e74cbbfb29534e25b262
Morten Victor Nordbye <morten@nordbye.it>
2026-04-04 14:35:12 +0200

    feat(auth): wrap workout_feeling type creation in a block

--- drizzle/0004_volatile_maddog.sql
+++ drizzle/0004_volatile_maddog.sql
@@ -1,2 +1,6 @@
-CREATE TYPE "public"."workout_feeling" AS ENUM('Tired', 'OK', 'Good', 'Awesome');--> statement-breakpoint
+DO $$ BEGIN
+  CREATE TYPE "public"."workout_feeling" AS ENUM('Tired', 'OK', 'Good', 'Awesome');
+EXCEPTION WHEN duplicate_object THEN NULL;
+END $$;
+--> statement-breakpoint
 ALTER TABLE "workout_sessions" ADD COLUMN "feeling" "workout_feeling";
